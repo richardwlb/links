ff7091fd8d713389aece82a7767b3ea011be7e07
Richard W. L. Brehmer <richardbrehmer@Richards-MacBook-Pro.local>
2020-07-01 21:51:34 -0300

    sequelize

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,18 @@
 const express = require('express');
+const db = require('./models');
+
+const authController = require('./controllers/auth');
 
 const app = express();
+
+app.use('/auth', authController);
 
 app.get('/', (req, res) => {
     return res.json('Hello!')
 });
 
-app.listen(3001, () => {
-    console.log('listening on port 3001');
+db.sequelize.sync().then( ()=> {
+    app.listen(3001, () => {
+        console.log('listening on port 3001');
+    });
 });
