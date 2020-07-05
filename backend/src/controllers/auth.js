bc1793aeb08a384aa22daff9e49a7c71832d9934
Richard W. L. Brehmer <richardbrehmer@Richards-MacBook-Pro.local>
2020-07-04 23:07:44 -0300

    account model

--- backend/src/controllers/auth.js
+++ backend/src/controllers/auth.js
@@ -1,13 +1,27 @@
 const express = require('express');
+const bcrypt = require('bcrypt');
+const { Account } = require('../models');
 
 const router = express.Router();
+
+const saltRounds = 10;
 
 router.get('/sign-in', (req, res) => {
     return res.json('sign-in');
 });
 
-router.get('/sign-up', (req, res) => {
-    return res.json('sign up');
+router.get('/sign-up', async (req, res) => {
+
+    const email = 'richard@bnu.com.br';
+    const password = '1020';
+
+    const hash = bcrypt.hashSync(password, saltRounds);
+    console.log(hash);
+
+    const result = await Account.create({email, password: hash});
+
+    return res.json(result);
+
 });
 
 module.exports = router;
